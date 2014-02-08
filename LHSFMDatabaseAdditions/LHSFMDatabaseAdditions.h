b8abb5e9303b0b7c8a309c2f2d706bb0a7534bf5
Dan Loewenherz <dloewenherz@gmail.com>
2014-02-08 15:32:08 -0600

    add relevant files

---
+++ LHSFMDatabaseAdditions/LHSFMDatabaseAdditions.h
@@ -0,0 +1,13 @@
+//
+//  LHSFMDatabaseAdditions.h
+//  LHSFMDatabaseAdditions
+//
+//  Created by Dan Loewenherz on 2/8/14.
+//  Copyright (c) 2014 Lionheart Software LLC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LHSFMDatabaseAdditions : NSObject
+
+@end
---
+++ LHSFMDatabaseAdditions/LHSFMDatabaseAdditions.m
@@ -0,0 +1,13 @@
+//
+//  LHSFMDatabaseAdditions.m
+//  LHSFMDatabaseAdditions
+//
+//  Created by Dan Loewenherz on 2/8/14.
+//  Copyright (c) 2014 Lionheart Software LLC. All rights reserved.
+//
+
+#import "LHSFMDatabaseAdditions.h"
+
+@implementation LHSFMDatabaseAdditions
+
+@end
